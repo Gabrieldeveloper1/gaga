170b66f6090f10caf9f07cf84a9eb661d67770f0
Gabriel Kramer <kramermota55@gmail.com>
2023-06-08 22:02:53 -0300

    Portfolio mais completo e com responsividade 95%

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,28 @@
-const nome = "Gabriel"
+
+const menu = document.querySelector(".menu");
+const NavMenu = document.querySelector(".nav-menu");
+menu.addEventListener("click", () => {
+  menu.classList.toggle("ativo");
+  NavMenu.classList.toggle("ativo");
+});
+
+new TypeIt(".name-title", {
+  strings: "",
+  speed: 75,
+  loop: false,
+}).go();
+
+new TypeIt("#asyncExec", {
+  waitUntilVisible: true,
+})
+  .type("Frontend")
+  .exec(async () => {
+    //-- Return a promise that resolves after something happens.
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        return resolve();
+      }, 2500);
+    });
+  })
+  .type(" Developer")
+  .go();
